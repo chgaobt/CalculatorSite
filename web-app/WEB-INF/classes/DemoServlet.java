623c93d791ce45ccfd9b54960096d0063a279cd4
CGao2023 <chloejgao@gmail.com>
2023-02-16 17:25:57 -0500

    servlet tutorial start

--- web-app/WEB-INF/classes/DemoServlet.java
+++ web-app/WEB-INF/classes/DemoServlet.java
@@ -12,6 +12,6 @@ public class DemoServlet extends HttpServlet{
         out.println("Welcome to Servlet");
         out.println("</body></html>");
 
-        out.close(); 
+        out.close();
     }
 }
